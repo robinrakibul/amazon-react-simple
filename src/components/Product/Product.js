d818015e21dcfac25970f06670aa139a4f1e2be1
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-22 00:19:34 +0600

    product.js almost finished, added all the info

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,7 +6,13 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'><p>Add to Cart</p></button>
         </div>
     );
 };
