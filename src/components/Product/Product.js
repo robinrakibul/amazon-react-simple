8433c1a9ca73a292744b1a7dcd920509421a79fa
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-23 00:01:08 +0600

    calculation fixed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,10 @@
-import { faCartPlus, faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
-const Product = (props) => {
-    const {product, handleAddToCart} = props;
+const Product = ({product, handleAddToCart}) => {
+    // const {product, handleAddToCart} = props;
     const { name, img, seller, price, ratings } = product;
     
     return (
@@ -18,7 +18,7 @@ const Product = (props) => {
             </div>
             <button onClick={() => handleAddToCart(product)} className='btn-cart'>
                 <p className='btn-text'>Add to Cart</p>
-                <FontAwesomeIcon icon={faCartPlus}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
